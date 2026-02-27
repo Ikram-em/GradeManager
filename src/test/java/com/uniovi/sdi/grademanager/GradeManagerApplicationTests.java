bf973265f131fee88e08015f3eb1d9693a1762af
Ikram El Mabrouk Morhnane <ikramaaa57@gmail.com>
2026-02-27 12:10:21 +0100

    SDI-2526-704-5.5-Casos Uso VistaSignUp.

--- src/test/java/com/uniovi/sdi/grademanager/GradeManagerApplicationTests.java
+++ src/test/java/com/uniovi/sdi/grademanager/GradeManagerApplicationTests.java
@@ -2,6 +2,7 @@ package com.uniovi.sdi.grademanager;
 
 import com.uniovi.sdi.grademanager.pageobjects.PO_HomeView;
 import com.uniovi.sdi.grademanager.pageobjects.PO_Properties;
+import com.uniovi.sdi.grademanager.pageobjects.PO_SignUpView;
 import org.junit.jupiter.api.AfterAll;
 import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.Assertions;
@@ -109,5 +110,38 @@ class GradeManagerApplicationTests {
     @Order(5)
     void PR04() {
         PO_HomeView.checkChangeLanguage(driver, "btnSpanish", "btnEnglish", PO_Properties.getSPANISH(), PO_Properties.getENGLISH());
+    }
+
+    // PR05. Prueba del formulario de registro. registro con datos correctos
+    @Test
+    @Order(6)
+    public void PR05() {
+        PO_HomeView.clickOption(driver, "signup", "class", "btn btn-primary");
+        PO_SignUpView.fillForm(driver, "77777778A", "Josefo", "Perez", "77777", "77777");
+        String checkText = "Notas del usuario";
+        List<WebElement> result = PO_HomeView.checkElementBy(driver, "text", checkText);
+        Assertions.assertEquals(checkText, result.getFirst().getText());
+    }
+
+    // PR06A. Prueba del formulario de registro. DNI repetido en la BD
+    @Test
+    @Order(7)
+    public void PR06A() {
+        PO_HomeView.clickOption(driver, "signup", "class", "btn btn-primary");
+        PO_SignUpView.fillForm(driver, "99999990A", "Josefo", "Perez", "77777", "77777");
+        List<WebElement> result = PO_SignUpView.checkElementByKey(driver, "Error.signup.dni.duplicate", PO_Properties.getSPANISH());
+        String checkText = PO_HomeView.getP().getString("Error.signup.dni.duplicate", PO_Properties.getSPANISH());
+        Assertions.assertEquals(checkText, result.getFirst().getText());
+    }
+
+    // PR06B. Prueba del formulario de registro. Nombre corto.
+    @Test
+    @Order(8)
+    public void PR06B() {
+        PO_HomeView.clickOption(driver, "signup", "class", "btn btn-primary");
+        PO_SignUpView.fillForm(driver, "99999990B", "Jose", "Perez", "77777", "77777");
+        List<WebElement> result = PO_SignUpView.checkElementByKey(driver, "Error.signup.name.length", PO_Properties.getSPANISH());
+        String checkText = PO_HomeView.getP().getString("Error.signup.name.length", PO_Properties.getSPANISH());
+        Assertions.assertEquals(checkText, result.getFirst().getText());
     }
 }
--- src/test/java/com/uniovi/sdi/grademanager/pageobjects/PO_SignUpView.java
+++ src/test/java/com/uniovi/sdi/grademanager/pageobjects/PO_SignUpView.java
@@ -2,23 +2,37 @@ package com.uniovi.sdi.grademanager.pageobjects;
 
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
 
 public class PO_SignUpView extends PO_NavView {
 
-    public static void fillForm(WebDriver driver, String dni, String name, String lastName, String password, String confirmPassword) {
-        driver.findElement(By.xpath("//input[@name='dni']")).clear();
-        driver.findElement(By.xpath("//input[@name='dni']")).sendKeys(dni);
-        driver.findElement(By.xpath("//input[@name='name']")).clear();
-        driver.findElement(By.xpath("//input[@name='name']")).sendKeys(name);
-        driver.findElement(By.xpath("//input[@name='lastName']")).clear();
-        driver.findElement(By.xpath("//input[@name='lastName']")).sendKeys(lastName);
-        driver.findElement(By.xpath("//input[@name='password']")).clear();
-        driver.findElement(By.xpath("//input[@name='password']")).sendKeys(password);
-        driver.findElement(By.xpath("//input[@name='passwordConfirm']")).clear();
-        driver.findElement(By.xpath("//input[@name='passwordConfirm']")).sendKeys(confirmPassword);
-    }
+    public static void fillForm(WebDriver driver, String dnip, String namep, String lastnamep, String passwordp, String passwordconfp) {
+        WebElement dni = driver.findElement(By.name("dni"));
+        dni.click();
+        dni.clear();
+        dni.sendKeys(dnip);
 
-    public static void submit(WebDriver driver) {
-        checkElementBy(driver, "free", "//button[@type='submit']").get(0).click();
+        WebElement name = driver.findElement(By.name("name"));
+        name.click();
+        name.clear();
+        name.sendKeys(namep);
+
+        WebElement lastname = driver.findElement(By.name("lastName"));
+        lastname.click();
+        lastname.clear();
+        lastname.sendKeys(lastnamep);
+
+        WebElement password = driver.findElement(By.name("password"));
+        password.click();
+        password.clear();
+        password.sendKeys(passwordp);
+
+        WebElement passwordConfirm = driver.findElement(By.name("passwordConfirm"));
+        passwordConfirm.click();
+        passwordConfirm.clear();
+        passwordConfirm.sendKeys(passwordconfp);
+
+        By boton = By.className("btn");
+        driver.findElement(boton).click();
     }
 }
