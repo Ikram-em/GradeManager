930f97f2b3b54969574c9939f2d9862838b2bf36
Ikram El Mabrouk Morhnane <ikramaaa57@gmail.com>
2026-02-27 12:08:52 +0100

    SDI-2526-704-5.4-Casos Uso VistaHome.

--- src/test/java/com/uniovi/sdi/grademanager/pageobjects/PO_HomeView.java
+++ src/test/java/com/uniovi/sdi/grademanager/pageobjects/PO_HomeView.java
@@ -1,12 +1,26 @@
 package com.uniovi.sdi.grademanager.pageobjects;
 
+import com.uniovi.sdi.grademanager.util.SeleniumUtils;
 import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+
+import java.util.List;
 
 public class PO_HomeView extends PO_NavView {
 
+    public static void checkWelcomeToPage(WebDriver driver, int language) {
+        SeleniumUtils.waitLoadElementsBy(driver, "text", p.getString("welcome.message", language), getTimeout());
+    }
 
-    //Se hizo en el commit anteriro sin querer.
-    public static void checkWelcomeMessage(WebDriver driver, int locale) {
-        checkElementByKey(driver, "welcome.message", locale);
+    public static List<WebElement> getWelcomeMessageText(WebDriver driver, int language) {
+        return SeleniumUtils.waitLoadElementsBy(driver, "text", p.getString("welcome.message", language), getTimeout());
+    }
+
+    public static void checkChangeLanguage(WebDriver driver, String textLanguage1, String textLanguage, int locale1, int locale2) {
+        PO_HomeView.checkWelcomeToPage(driver, locale1);
+        PO_HomeView.changeLanguage(driver, textLanguage);
+        PO_HomeView.checkWelcomeToPage(driver, locale2);
+        PO_HomeView.changeLanguage(driver, textLanguage1);
+        PO_HomeView.checkWelcomeToPage(driver, locale1);
     }
 }
